d609114e82f0c27b7707b21939e549dc061b1f32
Tpt <thomaspt@hotmail.fr>
2014-10-12 19:27:33 +0200

    Adds basics for simplifiers

---
+++ src/SentenceTreeSimplifier/SimplifierException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PPP\Wikidata\SentenceTreeSimplifier;
+
+use Exception;
+
+/**
+ * Exception thrown if things go wrong during the simplification.
+ *
+ * @licence GPLv2+
+ * @author Thomas Pellissier Tanon
+ */
+class SimplifierException extends Exception {
+}
