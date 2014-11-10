096534166298eddfe49cc8506d3a7364b759d41f
Tpt <thomaspt@hotmail.fr>
2014-11-10 12:30:09 +0100

    Better formatting of ISO 8601 time

--- src/ValueFormatters/TimeFormatter.php
+++ src/ValueFormatters/TimeFormatter.php
@@ -11,8 +11,7 @@ use ValueParsers\TimeParser;
 /**
  * @licence GPLv2+
  * @author Thomas Pellissier Tanon
- *
- * @todo cut time representation at the precision
+ * @todo Move ISO formatting to data-value/Time
  */
 class TimeFormatter extends ValueFormatterBase {
 
@@ -29,11 +28,80 @@ class TimeFormatter extends ValueFormatterBase {
 			throw new InvalidArgumentException('DataValue is not a TimeValue.');
 		}
 
-		return new TimeResourceNode($this->simplifyIsoTime($value->getTime()), $this->getCalendarName($value->getCalendarModel()));
+		return new TimeResourceNode($this->simplifyIsoTime($value), $this->getCalendarName($value->getCalendarModel()));
 	}
 
-	private function simplifyIsoTime($time) {
-		return preg_replace('/^\+0+/', '', preg_replace('/^\-0+/', '-', $time));
+	private function simplifyIsoTime(TimeValue $value) {
+		$parts = $this->explodeISOTime($value->getTime());
+		$precision = $value->getPrecision();
+
+		$iso = '';
+		if($parts['year'] < 0) {
+			$iso .= '-' . $this->toStringOfSize(-1 * $parts['year'], 4);
+		} else {
+			$iso .= $this->toStringOfSize($parts['year'], 4);
+		}
+
+		//Month
+		if($precision < TimeValue::PRECISION_MONTH) {
+			return $iso;
+		}
+		$iso .= '-' . $this->toStringOfSize($parts['month'], 2);
+
+		//Day
+		if($precision < TimeValue::PRECISION_DAY) {
+			return $iso;
+		}
+		$iso .= '-' . $this->toStringOfSize($parts['day'], 2);
+
+		//Hours
+		if($precision < TimeValue::PRECISION_HOUR) {
+			return $iso;
+		}
+		$iso .= 'T' . $this->toStringOfSize($parts['hour'], 2);
+
+		//Minutes
+		if($precision < TimeValue::PRECISION_MINUTE) {
+			return $iso . $this->timezoneToIso($value->getTimezone());
+		}
+		$iso .= ':' . $this->toStringOfSize($parts['minute'], 2);
+
+		//Seconds
+		if($precision < TimeValue::PRECISION_SECOND) {
+			return $iso . $this->timezoneToIso($value->getTimezone());
+		}
+		$iso .= ':' . $this->toStringOfSize($parts['second'], 2);
+
+		return $iso . $this->timezoneToIso($value->getTimezone());
+	}
+
+	private function toStringOfSize($number, $size) {
+		return str_pad((int) $number, $size, '0', STR_PAD_LEFT);
+	}
+
+	private function timezoneToIso($timezone) {
+		if($timezone === 0) {
+			return 'Z';
+		} elseif($timezone > 0) {
+			return '+' . $this->toStringOfSize($timezone / 60, 2) . ':' . $this->toStringOfSize($timezone % 60, 2);
+		} else {
+			return '-' . $this->toStringOfSize((-1 * $timezone) / 60, 2) . ':' . $this->toStringOfSize((-1 * $timezone) % 60, 2);
+		}
+	}
+
+	private function explodeISOTime($time) {
+			if(!preg_match('/([+-]\d+)-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z/', $time, $m)) {
+			throw new InvalidArgumentException('$time is not a valid time');
+		}
+
+		return array(
+			'year' => intval($m[1]),
+			'month' => intval($m[2]),
+			'day' => intval($m[3]),
+			'hour' => intval($m[4]),
+			'minute' => intval($m[5]),
+			'second' => intval($m[6])
+		);
 	}
 
 	private function getCalendarName($calendarModel) {
