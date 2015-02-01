faa4b7f0d6df98a2d3bd4c283168a7a0c762940b
Tpt <thomaspt@hotmail.fr>
2015-02-01 12:42:57 +0100

    Adds support for more Wikipedias (closes #82)

--- www/index.php
+++ www/index.php
@@ -25,9 +25,18 @@ if(class_exists('Memcached')) {
 $entryPoint = new ModuleEntryPoint(new WikidataRequestHandler(
 	'https://www.wikidata.org/w/api.php',
 	array(
-		'enwiki' => 'http://en.wikipedia.org/w/api.php',
+		'arwiki' => 'http://ar.wikipedia.org/w/api.php',
 		'dewiki' => 'http://de.wikipedia.org/w/api.php',
-		'frwiki' => 'http://fr.wikipedia.org/w/api.php'
+		'enwiki' => 'http://en.wikipedia.org/w/api.php',
+		'eswiki' => 'http://es.wikipedia.org/w/api.php',
+		'frwiki' => 'http://fr.wikipedia.org/w/api.php',
+		'itwiki' => 'http://it.wikipedia.org/w/api.php',
+		'jawiki' => 'http://ja.wikipedia.org/w/api.php',
+		'nlwiki' => 'http://nl.wikipedia.org/w/api.php',
+		'plwiki' => 'http://pl.wikipedia.org/w/api.php',
+		'ptwiki' => 'http://pt.wikipedia.org/w/api.php',
+		'ruwiki' => 'http://ru.wikipedia.org/w/api.php',
+		'zhwiki' => 'http://zh.wikipedia.org/w/api.php'
 	),
 	'https://wdq.wmflabs.org/api',
 	$cache
